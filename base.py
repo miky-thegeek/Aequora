6dcb3eb8f6a60546972d3f8c684751e20722e13e
Michele Giorgio <tecnomiky+github@gmail.com>
2025-03-17 18:09:16 +0100

    draft with code for join transaction with card and skeleton of web page to review transaction

---
+++ base.py
@@ -0,0 +1,80 @@
+import csv
+import re
+from datetime import datetime
+
+def getCardTransaction(csvCard, dateBank, descPartsBank):
+    rowNumberCard = 0
+    #fileCard.seek(0)
+                        
+    for lineCard in csvCard:
+        #print(lineCard)
+        descPartsCard = re.split(r'\s{2,}', lineCard[3])
+
+        if rowNumberCard > 0:
+
+            datetime_card = datetime.strptime(lineCard[0], '%d/%m/%Y')
+
+            #print("datetime_card: "+str(datetime_card))
+
+            if dateBank == datetime_card:
+                timeCard = datetime.strptime(lineCard[1], '%H:%M')
+                datetime_card = datetime_card.replace(hour=timeCard.hour, minute=timeCard.minute)
+
+                if descPartsCard[0] == descPartsBank[3]:
+
+                    #print(str(datetime_card)+" "+lineCard[3])
+                    print(str(datetime_card)+" "+descPartsCard[0]+" "+descPartsCard[1])
+                    otherAccounts.add(descPartsCard[0])
+
+        rowNumberCard += 1
+
+
+with open('/home/michele/Downloads/Elenco_Movimenti(2).csv', mode ='r') as fileBank:
+    csvFileBank = csv.reader(fileBank, delimiter=';')
+
+    with open('/home/michele/Downloads/Elenco_Movimenti(1).csv', mode ='r') as fileCard:
+        csvFileCard = csv.reader(fileCard, delimiter=';')
+
+        with open('/home/michele/Downloads/L5BXD33J8AZMG-CSR-20230101000000-20250315235959-20250316052428.CSV', mode ='r') as filePayPal:
+            csvFilePayPal = csv.reader(filePayPal)
+
+            rowNumberBank = 0
+            otherAccounts = set()
+            for lineBank in csvFileBank:
+
+                if rowNumberBank > 0:
+
+                    datetimeBank = datetime.strptime(lineBank[1], '%d.%m.%Y')
+                    descPartsBank = re.split(r'\s{2,}', lineBank[2])
+
+                    if "MASTERCARD" in lineBank[2]:
+                        print("datetime_bank: "+str(datetimeBank))
+
+                        #rowNumberCard = 0
+                        fileCard.seek(0)
+
+                        getCardTransaction(csvFileCard, datetimeBank, descPartsBank)
+                        
+                        #for lineCard in csvFileCard:
+                            #print(lineCard)
+                            #descPartsCard = re.split(r'\s{2,}', lineCard[3])
+
+                            #if rowNumberCard > 0:
+
+                                #datetime_card = datetime.strptime(lineCard[0], '%d/%m/%Y')
+
+                                #print("datetime_card: "+str(datetime_card))
+
+                                #if datetimeBank == datetime_card:
+                                    #timeCard = datetime.strptime(lineCard[1], '%H:%M')
+                                    #datetime_card = datetime_card.replace(hour=timeCard.hour, minute=timeCard.minute)
+
+                                    #if descPartsCard[0] == descPartsBank[3]:
+
+                                        #print(str(datetime_card)+" "+lineCard[3])
+                                        #print(str(datetime_card)+" "+descPartsCard[0]+" "+descPartsCard[1])
+                                        #otherAccounts.add(descPartsCard[0])
+
+                            #rowNumberCard += 1
+                rowNumberBank += 1
+            print(otherAccounts)
