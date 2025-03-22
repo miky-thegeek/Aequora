78c463099e3bdd41bd22395c5dce89a6b5e29019
Michele Giorgio <tecnomiky+github@gmail.com>
2025-03-22 18:58:32 +0100

    Added date cobersion with pandas

--- base.py
+++ base.py
@@ -1,57 +1,60 @@
-import csv
 import re
 import pandas
 import compute_next_business_day
-from datetime import datetime, timedelta
+import decimal
+from datetime import datetime
 
 def getCardTransaction(csvCard, dateBank, descPartsBank):
                         
     for lineCard in csvCard.itertuples():
         #print(lineCard)
         descPartsCard = re.split(r'\s{2,}', lineCard[4])
+        #datetime_card = datetime.strptime(lineCard[1], '%d/%m/%Y')
+        #datetime_card = lineCard[1].to_pydatetime()
 
-        datetime_card = datetime.strptime(lineCard[1], '%d/%m/%Y')
-
-        #print("datetime_card: "+str(datetime_card))
-
-        if dateBank == datetime_card:
-            timeCard = datetime.strptime(lineCard[2], '%H:%M')
-            datetime_card = datetime_card.replace(hour=timeCard.hour, minute=timeCard.minute)
+        if dateBank == lineCard[1]:
+            datetime_card = lineCard[1].to_pydatetime()
+            #timeCard = datetime.strptime(lineCard[2], '%H:%M')
+            #timeCard = lineCard[2].to_pydatetime()
+            datetime_card = datetime_card.replace(hour=lineCard[2].hour, minute=lineCard[2].minute)
 
             if descPartsCard[0] == descPartsBank[3]:
 
                 #print(str(datetime_card)+" "+lineCard[3])
                 print(str(datetime_card)+" "+descPartsCard[0]+" "+descPartsCard[1])
-                otherAccounts.add(descPartsCard[0])
+                #otherAccounts.add(descPartsCard[0])
 
+#def getPayPalTransaction(csvPayPal)
 
-csvFileBank = pandas.read_csv('/home/michele/Downloads/Elenco_Movimenti(2).csv', decimal=",", sep=';')
+csvFileBank = pandas.read_csv('/home/michele/Downloads/Elenco_Movimenti(2).csv', decimal=",", sep=';', parse_dates=[0, 1], dayfirst=True)
 
-csvFileCard = pandas.read_csv('/home/michele/Downloads/Elenco_Movimenti(1).csv', decimal=',', sep=';')
+csvFileCard = pandas.read_csv('/home/michele/Downloads/Elenco_Movimenti(1).csv', decimal=',', sep=';', parse_dates=[0, 1, 2], dayfirst=True)
 
-csvFilePayPal = pandas.read_csv('/home/michele/Downloads/L5BXD33J8AZMG-CSR-20230101000000-20250315235959-20250316052428.CSV', decimal=',')
+csvFilePayPal = pandas.read_csv('/home/michele/Downloads/L5BXD33J8AZMG-CSR-20230101000000-20250315235959-20250316052428.CSV', decimal=',', parse_dates=[0, 1], dayfirst=True)
 
 otherAccounts = set()
 for lineBank in csvFileBank.itertuples():
 
-    datetimeBank = datetime.strptime(lineBank[2], '%d.%m.%Y')
+    #datetimeBank = datetime.strptime(lineBank[2], '%d.%m.%Y')
     descPartsBank = re.split(r'\s{2,}', lineBank[3])
 
-    print("datetime_bank: "+str(datetimeBank))
+    #csvFileBank.at[lineBank[0], 'Importo (EUR)'] = float(lineBank[4].replace(',', '.'))
+
+    print("datetime_bank: "+str(lineBank[2].to_pydatetime()))
 
     if "MASTERCARD" in lineBank[3]:
 
-        getCardTransaction(csvFileCard, datetimeBank, descPartsBank)
+        getCardTransaction(csvFileCard, lineBank[2], descPartsBank)
         
     elif "PayPal" in lineBank[3]:
 
         idDataset = -1
 
         for linePayPal in csvFilePayPal.itertuples():
-            datetimePayPal = datetime.strptime(linePayPal[1], '%d/%m/%Y')
+            #datetimePayPal = datetime.strptime(linePayPal[1], '%d/%m/%Y')
 
             for daysNumber in [2, 3, 1, 0]:
-                if abs(float(lineBank[4].replace(',', '.'))) == abs(linePayPal[6]) and compute_next_business_day.next_number_business_day(datetimePayPal, 'IT', daysNumber) == datetimeBank:
+                if abs(float(lineBank[4].replace(',', '.'))) == abs(linePayPal[6]) and compute_next_business_day.next_number_business_day(linePayPal[1].to_pydatetime(), 'IT', daysNumber) == lineBank[2].to_pydatetime():
                     print(linePayPal)
                     idDataset = linePayPal[0]
                 
