15b888f901126c79be51f30ce05d1ee6eddac97c
Michele Giorgio <tecnomiky+github@gmail.com>
2025-03-22 19:10:02 +0100

    Added function to elaborate PayPal transaction and clened old code

--- base.py
+++ base.py
@@ -9,13 +9,9 @@ def getCardTransaction(csvCard, dateBank, descPartsBank):
     for lineCard in csvCard.itertuples():
         #print(lineCard)
         descPartsCard = re.split(r'\s{2,}', lineCard[4])
-        #datetime_card = datetime.strptime(lineCard[1], '%d/%m/%Y')
-        #datetime_card = lineCard[1].to_pydatetime()
 
         if dateBank == lineCard[1]:
             datetime_card = lineCard[1].to_pydatetime()
-            #timeCard = datetime.strptime(lineCard[2], '%H:%M')
-            #timeCard = lineCard[2].to_pydatetime()
             datetime_card = datetime_card.replace(hour=lineCard[2].hour, minute=lineCard[2].minute)
 
             if descPartsCard[0] == descPartsBank[3]:
@@ -24,7 +20,20 @@ def getCardTransaction(csvCard, dateBank, descPartsBank):
                 print(str(datetime_card)+" "+descPartsCard[0]+" "+descPartsCard[1])
                 #otherAccounts.add(descPartsCard[0])
 
-#def getPayPalTransaction(csvPayPal)
+def getPayPalTransaction(csvPayPal, ammount_bank, date_bank):
+    idDataset = -1
+
+    for linePayPal in csvPayPal.itertuples():
+
+        for daysNumber in [2, 3, 1, 0]:
+            if abs(ammount_bank) == abs(linePayPal[6]) and compute_next_business_day.next_number_business_day(linePayPal[1].to_pydatetime(), 'IT', daysNumber) == date_bank:
+                print(linePayPal)
+                idDataset = linePayPal[0]
+            
+            if idDataset != -1:
+                break
+        if idDataset != -1:
+                break
 
 csvFileBank = pandas.read_csv('/home/michele/Downloads/Elenco_Movimenti(2).csv', decimal=",", sep=';', parse_dates=[0, 1], dayfirst=True)
 
@@ -35,7 +44,6 @@ csvFilePayPal = pandas.read_csv('/home/michele/Downloads/L5BXD33J8AZMG-CSR-20230
 otherAccounts = set()
 for lineBank in csvFileBank.itertuples():
 
-    #datetimeBank = datetime.strptime(lineBank[2], '%d.%m.%Y')
     descPartsBank = re.split(r'\s{2,}', lineBank[3])
 
     #csvFileBank.at[lineBank[0], 'Importo (EUR)'] = float(lineBank[4].replace(',', '.'))
@@ -48,19 +56,6 @@ for lineBank in csvFileBank.itertuples():
         
     elif "PayPal" in lineBank[3]:
 
-        idDataset = -1
+        getPayPalTransaction(csvFilePayPal, float(lineBank[4].replace(',', '.')), lineBank[2].to_pydatetime())
 
-        for linePayPal in csvFilePayPal.itertuples():
-            #datetimePayPal = datetime.strptime(linePayPal[1], '%d/%m/%Y')
-
-            for daysNumber in [2, 3, 1, 0]:
-                if abs(float(lineBank[4].replace(',', '.'))) == abs(linePayPal[6]) and compute_next_business_day.next_number_business_day(linePayPal[1].to_pydatetime(), 'IT', daysNumber) == lineBank[2].to_pydatetime():
-                    print(linePayPal)
-                    idDataset = linePayPal[0]
-                
-                if idDataset != -1:
-                    break
-            if idDataset != -1:
-                    break
-                
     #print(otherAccounts)
